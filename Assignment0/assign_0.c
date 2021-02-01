8acd0f6b18e032de7ea26b507484905f6ae39bc3
lee0717 <lee0717@iastate.edu>
2021-02-01 12:35:24 +0800

    Lee's part

---
+++ Assignment0/assign_0.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void initializeSandPile(int sandPile[23][23]);
+void outputSandPile(int sandPile[23][23]);
+
+int main(int argc, char *argv[])
+{
+    //variable
+    int sandPile[23][23];
+    int fps;
+
+    //initialize sandPile
+    initializeSandPile(sandPile);
+
+    //if there is argument
+    if(argc>1)
+    {
+
+        //variable
+        int i;
+        int corx;
+        int cory;
+        int h;
+
+        //if there is fps in argument
+        if(!strcmp("--fps",argv[1])){
+            fps = atoi(argv[2]);
+            for(i =1; i<argc;i++){
+                if(i%3 ==0){
+                    corx = atoi(argv[i-2+2]);
+                    cory = atoi(argv[i-1+2]);
+                    h = atoi(argv[i+2]);
+                    sandPile[corx][cory] = h;
+                }
+            }
+        }
+
+        //if there is no fps in argument
+        else{
+            fps = 60; //default 60 fps
+            for(i =1; i<argc;i++){
+                if(i%3 ==0){
+                    corx = atoi(argv[i-2]);
+                    cory = atoi(argv[i-1]);
+                    h = atoi(argv[i]);
+                    sandPile[corx][cory] = h;
+                }
+            }
+        }
+    }
+
+    //output sandPile
+    outputSandPile(sandPile);
+}
+
+void initializeSandPile(int sandPile[23][23])
+{
+    int i,j;
+
+    for(j=0;j<23;j++){
+        for(i = 0;i<23;i++){
+            sandPile[j][i] = 0;
+        }
+    }
+}
+
+void outputSandPile(int sandPile[23][23])
+{
+    int i,j;
+
+    for(j=0;j<23;j++){
+        for(i = 0;i<23;i++){
+            if(sandPile[j][i]!=-1){
+                printf("%3d",sandPile[j][i]);
+            }
+            else{
+                printf("%3c",'#');
+            }
+        }
+        printf("\n");
+
+    }
+}
